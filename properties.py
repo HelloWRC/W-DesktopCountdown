bac24bb46b0a2a4d14a784c49b15f688db12d32a
WRC <hello_wrc@outlook.com>
2022-08-21 11:14:09 +0800

    0.6.0 beta

--- properties.py
+++ properties.py
@@ -1,6 +1,6 @@
-version = '0.5.5 beta'
-version_code = '0.5.5b'
-version_id = 50500
+version = '0.6.0 beta'
+version_code = '0.6.0b'
+version_id = 60000
 app_uuid = '721A9FA1-351A-B68A-AE4B-52E9B9460144'
 
 log_styles = '[%(asctime)s] [%(threadName)s/%(module)s.%(funcName)s(%(lineno)s)/%(levelname)s] %(message)s'
