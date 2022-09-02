f9e1df1f95bf091e00294fa150d29afe7e69986a
WRC <hello_wrc@outlook.com>
2022-09-02 21:32:05 +0800

    Update build script

--- Scripts/Build.bat
+++ Scripts/Build.bat
@@ -2,10 +2,9 @@
 cmd /c Ready.bat
 
 cd ..
-echo ----------正在生成exe文件----------
+echo ---------- Building EXE ----------
 pyinstaller -F warp.py -p . -w -i ./resources/icons/colorful/logo.ico
 cd dist
 del W-DesktopCountdown*.exe
-cd ..
-python -c "import os;import properties;os.rename('./dist/warp.exe', './dist/W-DesktopCountdown-{}.exe'.format(properties.version_code))"
-echo ----------生成完成----------
+ren ..\dist\warp.exe W-DesktopCountdown.exe
+echo ---------- COMPLETED ----------
